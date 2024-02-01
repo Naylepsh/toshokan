69348cb23c0b2fb558eb27cee8ef95d7501bc031
naylepsh <naylepsh@gmail.com>
2024-02-01 15:45:16 +0100

    make things kinda work

--- modules/library/src/main/scala/library/AssetRepository.scala
+++ modules/library/src/main/scala/library/AssetRepository.scala
@@ -22,7 +22,10 @@ trait AssetRepository[F[_], AssetId, AssetEntryId]:
   ): F[Either[AddEntryError, ExistingAssetEntry[AssetEntryId]]]
 
 object AssetRepository:
-  case class Column[A: Read: Put](name: String):
+  case class Column[A: Read: Put](rawName: String):
+    val name = Fragment.const0(f"${rawName}")
+    val sql = name
+
     def read = summon[Read[A]]
 
   object Column:
@@ -30,7 +33,7 @@ object AssetRepository:
       c => SingleFragment(fr"${c.name}")
     given [A]: Conversion[Column[A], Fragment] = c => fr"${c.name}"
 
-  case class Columns[A](sql: Fragment)
+  case class Columns[+A](sql: Fragment)
   object Columns:
     given Conversion[Columns[?], Fragment]          = _.sql
     given Conversion[Columns[?], SingleFragment[?]] = _.sql
@@ -44,30 +47,22 @@ object AssetRepository:
 
     def apply[A1, A2](t: (Column[A1], Column[A2])): Columns[(A1, A2)] =
       import Column.*
-      val a: Fragment = t._1
-      val b: Fragment = t._2
-      new Columns(concatFrags(List(a, b)))
+      val frags = t.productIterator.map(_.asInstanceOf[Column[?]].sql)
+      new Columns(concatFrags(Iterable.from(frags)))
 
-
-    // def apply[A1, A2](t: (Column[A1], Column[A2])): Columns[(A1, A2)] =
-    //   new Columns(
-    //     concatFrags(
-    //       Iterable.from(t.productIterator.map(_.asInstanceOf[Fragment]))
-    //     )
-    //   )
-    //
   class TableDefinition(private val rawName: String):
     val name = Fragment.const0(rawName)
   object TableDefinition:
     given Conversion[TableDefinition, Fragment] = table => fr"${table.name}"
-    given Conversion[TableDefinition, SingleFragment[Nothing]] = table => fr"${table.name}"
+    given Conversion[TableDefinition, SingleFragment[Nothing]] =
+      table => fr"${table.name}"
 
   extension (fragment: Fragment)
     inline def queryOf[A](col: Column[A])         = fragment.query[A](col.read)
     inline def queryOf[A: Read](cols: Columns[A]) = fragment.query[A]
 
   object Assets extends TableDefinition("assets"):
-    val id    = Column[Long]("name")
+    val id    = Column[Long]("id")
     val title = Column[AssetTitle]("title")
 
     val * = Columns((id, title))
