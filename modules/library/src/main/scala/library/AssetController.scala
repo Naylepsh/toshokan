6b939ee05394f70b33f938e07372ecacfb482e02
naylepsh <naylepsh@gmail.com>
2024-02-03 16:08:59 +0100

    cleaning up

--- modules/library/src/main/scala/library/AssetController.scala
+++ modules/library/src/main/scala/library/AssetController.scala
@@ -1,25 +1,19 @@
 package library
 
-import cats.{Monad, MonadError}
+import cats.Monad
 import cats.syntax.all.*
 import org.http4s.*
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
 import org.http4s.headers.*
 
-type MET[F[_]] = MonadError[F, Throwable]
-
-class AssetController[F[_]: Monad: MET, A](
+class AssetController[F[_]: Monad, A](
     service: AssetService[F],
     view: AssetView[F, A]
 )(using EntityEncoder[F, A]) extends Http4sDsl[F]:
   private val httpRoutes = HttpRoutes.of[F]:
     case GET -> Root =>
-      service.findAll.attempt.flatMap: 
-        case Right(assetsWithEntries) =>
-          Ok(view.render(assetsWithEntries), `Content-Type`(view.mediaType))
-        case Left(error) => 
-          println(error)
-          Ok("???")
+      service.findAll.flatMap: assetsWithEntries =>
+        Ok(view.render(assetsWithEntries), `Content-Type`(view.mediaType))
 
   val routes = Router("assets" -> httpRoutes)
