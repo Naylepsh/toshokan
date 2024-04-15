a7c899cd05f57028f06a8988bb9dc5403681a4fa
naylepsh <naylepsh@gmail.com>
2024-04-15 18:37:10 +0200

    fix: consider no entries found as error in mangadex scraper

--- modules/scraper/src/main/scala/scraper/sites/mangadex/MangadexScraper.scala
+++ modules/scraper/src/main/scala/scraper/sites/mangadex/MangadexScraper.scala
@@ -11,20 +11,22 @@ class MangadexScraper[F[_]: Monad](api: MangadexApi[F]) extends SiteScraper[F]:
     MangadexScraper.extractMangaId(uri) match
       case Left(error) => error.asLeft.pure
       case Right(mangaId) =>
-        api
-          .getMangaFeed(mangaId)
-          .map:
-            case Left(error) => ScrapeError.Other(error.toString).asLeft
-            case Right(feed) =>
-              feed
-                .data
-                .map: chapter =>
-                  EntryFound(
-                    EntryNo(chapter.attributes.chapter),
-                    EntryUri(chapter.url),
-                    DateUploaded(chapter.attributes.createdAt.value)
-                  )
-                .asRight
+        api.getMangaFeed(mangaId).map:
+          case Left(error) => ScrapeError.Other(error.toString).asLeft
+          case Right(feed) => extractEntries(feed)
+
+  private def extractEntries(feed: GetMangaFeedResponse) =
+    if feed.data.isEmpty then ScrapeError.NoEntriesFound.asLeft
+    else
+      feed
+        .data
+        .map: chapter =>
+          EntryFound(
+            EntryNo(chapter.attributes.chapter),
+            EntryUri(chapter.url),
+            DateUploaded(chapter.attributes.createdAt.value)
+          )
+        .asRight
 
 object MangadexScraper:
   private val mangaIdFromUriPattern = "^https://mangadex.org/title/(.+)$".r
