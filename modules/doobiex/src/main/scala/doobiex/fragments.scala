316f8feabb45eb3dde74e667eff93fd62637a083
naylepsh <naylepsh@gmail.com>
2024-06-26 12:30:28 +0200

    feat[doobiex]: add more typesafe updates

--- modules/doobiex/src/main/scala/doobiex/fragments.scala
+++ modules/doobiex/src/main/scala/doobiex/fragments.scala
@@ -4,13 +4,12 @@ import cats.syntax.all.*
 import cats.{Functor, Reducible}
 import doobie.*
 import doobie.implicits.*
-import doobie.util.pos.Pos
 import doobie.util.fragment.Fragment
 
 def insertInto[F[_]: Reducible: Functor, A <: TableDefinition](
     table: A,
     columns: F[A => (Fragment, Fragment)]
-)(using Pos): Fragment =
+): Fragment =
   val columnNames = columns
     .map(_(table))
     .mapIntercalate(fr0"", fr0", "):
@@ -31,3 +30,15 @@ def insertIntoReturning[F[_]: Reducible: Functor, A <: TableDefinition, B](
     returningColumns: A => Columns[B]
 ): Fragment =
   sql"${insertInto(table, insertColumns)} RETURNING ${returningColumns(table)}"
+
+def updateTable[F[_]: Reducible: Functor, A <: TableDefinition](
+    table: A,
+    columns: F[A => (Fragment, Fragment)]
+): Fragment =
+  val set = columns
+    .map(_(table))
+    .mapIntercalate(fr0"", fr0", "):
+      case (acc, (name, value)) =>
+        fr0"$acc$name=$value"
+
+  sql"UPDATE $table SET $set"
